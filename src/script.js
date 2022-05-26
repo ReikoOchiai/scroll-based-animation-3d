c19bf27d56bd08bc11bb46da8d3d7075795fb348
ReikoOchiai <lissage.23@gmail.com>
2022-05-26 23:51:34 +1000

    Particles and extra spin to spheres added

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import * as dat from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Debug
@@ -11,7 +12,10 @@ const parameters = {
   materialColor: "#ffeded",
 };
 
-gui.addColor(parameters, "materialColor");
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
+});
 
 /**
  * Base
@@ -30,10 +34,10 @@ const textureLoader = new THREE.TextureLoader();
 const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
 // gradientTexture.magFilter = THREE.NearestFilter;
 
-// const material = new THREE.MeshToonMaterial({
-//   color: parameters.materialColor,
-//   gradientMap: gradientTexture,
-// });
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
 const sphere1 = new THREE.Mesh(
   new THREE.SphereGeometry(1, 32, 25),
   new THREE.MeshNormalMaterial({ wireframe: true })
@@ -60,6 +64,37 @@ sphere3.position.x = 2;
 scene.add(sphere1, sphere2, sphere3);
 
 const sectionMeshes = [sphere1, sphere2, sphere3];
+
+/**
+ * Particles
+ */
+
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectDistance * 0.5 -
+    Math.random() * objectDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Light
@@ -127,9 +162,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener("scroll", () => {
   scrollY = window.scrollY;
+
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
+  }
 });
 
 /**
@@ -167,8 +216,8 @@ const tick = () => {
 
   // Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.12;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // Render
